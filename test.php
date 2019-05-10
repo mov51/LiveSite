e818f93eb0bf2e7f81462caf17c350ff4db60110
mov51 <mov51@mov51.net>
2019-05-10 18:09:50 -0400

    added test.php for style displaying https://mov51.net/test.php

---
+++ test.php
@@ -0,0 +1,33 @@
+<head>
+  <?php include'./include/head.php';
+  $pageTitle = 'testing please!';
+  ?>
+  <title><?= $pageTitle ?></title>
+</head>
+<body>
+  <div class="container-fluid">
+    <div class="row justify-content-center mt-3">
+      <div class="order-2 order-lg-1 col-lg-3 my-lg-3 col-10 mb-3 ">
+      </div>
+      <div class="order-1 order-lg-2 col-lg-6 mb-lg-0 col-10">
+        <!-- content here -->
+        <div class="article">
+          <p class="intro">
+            <code>class="intro"</code></br>
+            sets a paragraph in an article to font weight 700.
+          </p>
+          <p>
+            <code>class="article"</code></br>
+            the class article applies this style to the div.
+            </br>
+            </br>
+            <code>&lt;code&gt;&lt;/code&gt;</code></br>
+            displays code like this!
+          </p>
+        </div>
+      </div>
+      <div class="order-3 order-lg-3 col-lg-3 my-lg-3 col-10 mb-3">
+      </div>
+    </div>
+  </div>
+</body>
