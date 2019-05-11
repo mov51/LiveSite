31f07a00d8a637df0d484d81a815da5f26083260
mov51 <mov51@mov51.net>
2019-05-11 00:15:10 -0400

    continued to work on testing page

--- test.php
+++ test.php
@@ -13,16 +13,42 @@
         <!-- content here -->
         <div class="article">
           <p class="intro">
-            <code>class="intro"</code></br>
+            <code>class="intro"</code> </br>
             sets a paragraph in an article to font weight 700.
           </p>
           <p>
-            <code>class="article"</code></br>
+            <code>class="article"</code> </br>
             the class article applies this style to the div.
             </br>
             </br>
-            <code>&lt;code&gt;&lt;/code&gt;</code></br>
+            <code>&lt;code&gt;&lt;/code&gt;</code> </br>
             displays code like this!
+          </p>
+          <div class="box dark mr-2 ml-2">
+            <p>
+              <code>class="box dark"</code> </br>
+              this is an internal box, its set with the box class and adds just the backbround and border styles. it has all of the sub clases just like the article class.
+            </p>
+          </div>
+        </div>
+        <div class="article dark">
+          <p>
+            <code>class="article dark"</code> </br>
+            adding the dark class to an article makes the background dark and the text orange. </br>
+            this should change for all of the below elements.
+          </p>
+          <div class="box default mt-2 mr-2 ml-2">
+            <p>
+              <code>class="box"</code> </br>
+              this is an internal box, its set with the box class and adds just the backbround and border styles. it has all of the sub clases just like the article class.
+            </p>
+          </div>
+        </div>
+        <div class="article danger">
+          <p>
+            <code>class="article danger"</code> </br>
+            adding the warning class to an article makes the background orange and the text flat white. </br>
+            this should change for all of the below elements.
           </p>
         </div>
       </div>
